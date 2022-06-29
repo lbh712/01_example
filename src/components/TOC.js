2fca0080083ce373978a3cd5626c2d0de276314e
lbh712 <lbh712@naver.com>
2022-06-29 11:05:47 +0900

    ver1.0.1

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -7,7 +7,7 @@ class TOC extends Component {
                 <ul>
                     <li><a href="1.html">HTML</a></li>
                     <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JAVASCRIPT</a></li>
+                    <li><a href="3.html">Javascript</a></li>
                 </ul>
             </nav>
         )
