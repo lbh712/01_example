ec2eacab022403621c1498482134b247c5673e53
lbh712 <lbh712@naver.com>
2022-06-29 16:57:39 +0900

    1.0.3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,86 @@
-// import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
-import TOC from './components/TOC';
-import Subject from './components/Subject';
-import Contents from './components/Contents';
-
+import "./App.css";
+import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Subject from "./components/Subject";
+import Contents from "./components/Contents";
 
 class App extends Component {
   //생성자 for state
   constructor(props) {
     super(props);
     this.state = {
+
       //키밸류 형태의 구조
-      subject: { title: 'WEB', sub: 'World Wide Web' },
+      mode: "welcome",
+      selected_content_id: 2,
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
+      subject: { title: "WEB", desc: "World Wide Web" },
       contents: [
-        { id: 1, title: 'HTML', desc:'HTML is Hyper Text Markup Language.' },
-        { id: 2, title: 'CSS', desc:'CSS is for design' },
-        { id: 3, title: 'JavaScript', desc:'JavaScript is for interactive' },
+        { id: 1, title: "HTML", desc: "HTML is Hyper Text Markup Language." },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
       ]
     }
   }
   render() {
+    console.log("App render");
+    let _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if (this.state.mode === 'read') {
+      let i = 0;
+      while (i < this.state.contents.length) {
+        let data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+
+    }
+
     return (
       <>
-        {/* <Subject /> */}
         {/* state 에서 받아옴 */}
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-        <TOC data={this.state.contents}></TOC>
-        {/* <Contents title="HTML" desc="HTML is Hyper Text Markup Language."></Contents> */}
-        <Contents title={this.state.contents[0].title} desc={this.state.contents[0].desc}></Contents>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+
+        {/* setState+bind */}
+        {/* <header>
+          <h1><a href='/' onClick={function (e) {
+            e.preventDefault();
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header> */}
+
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}>
+        </Subject>
+
+        {/* ////////////////////////////////////////////////////////////////////////////////////////////////// */}
+
+        <TOC data={this.state.contents}
+          onChangePage={function (id) {
+            this.setState(
+              {
+                mode: 'read', selected_content_id: Number(id)
+              }
+            );
+          }.bind(this)}></TOC>
+
+        {/* ////////////////////////////////////////////////////////////////////////////////////////////////// */}
+
+        {/* <Content title="HTML" desc="HTML is Hyper Text Markup Language."></Content> */}
+        <Contents title={_title} desc={_desc}></Contents>
+        {/* <Content title={this.state.Content[0].title} desc={this.state.Content[0].desc}></Content> */}
 
       </>
     );
--- src/components/Contents.js
+++ src/components/Contents.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 class Contents extends Component {
     render() {
+        console.log('Contents render');
         return (
+            // <article>
+            //     <h2>{this.props.title}</h2>
+            //     {this.props.desc}
+            // </article>
+
             <article>
-                <h2>{this.props.title}</h2>
+                <h2><a href='/' onClick={function (e) {
+                    e.preventDefault();
+                    this.props.onChangePage();
+                }.bind(this)}>{this.props.title}</a></h2>
                 {this.props.desc}
             </article>
         )
