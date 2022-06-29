ac26a2276bff025466fd7e3d9549950e725d9f76
lbh712 <lbh712@naver.com>
2022-06-29 13:38:16 +0900

    1.0.2

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ import Contents from './components/Contents';
 
 
 class App extends Component {
+  //생성자 for state
+  constructor(props) {
+    super(props);
+    this.state = {
+      //키밸류 형태의 구조
+      subject: { title: 'WEB', sub: 'World Wide Web' },
+      contents: [
+        { id: 1, title: 'HTML', desc:'HTML is Hyper Text Markup Language.' },
+        { id: 2, title: 'CSS', desc:'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc:'JavaScript is for interactive' },
+      ]
+    }
+  }
   render() {
     return (
       <>
         {/* <Subject /> */}
-        <Subject title="Web" sub="World Wide Web!"></Subject>
-        <TOC></TOC>
-        <Contents title="HTML" desc="HTML is Hyper Text Markup Language."></Contents>
+        {/* state 에서 받아옴 */}
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        {/* <Contents title="HTML" desc="HTML is Hyper Text Markup Language."></Contents> */}
+        <Contents title={this.state.contents[0].title} desc={this.state.contents[0].desc}></Contents>
+
       </>
     );
   }
